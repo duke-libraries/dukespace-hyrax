164e3cfdd33ed729063054c35f9c4a2ea1920202
Ayse Durmaz <ad96@SCE-ML-01.local>
2017-08-03 15:34:32 -0400

    (formatting)

--- lib/scholrax/importer/works_folder_importer.rb
+++ lib/scholrax/importer/works_folder_importer.rb
@@ -1,25 +1,25 @@
+require "pathname"
+
 module Scholrax::Importer
   class WorksFolderImporter
 
-  require "pathname"
+    attr_reader :admin_set_id, :export_top_folder, :metadata_mapping_file
 
-	attr_reader :admin_set_id, :export_top_folder, :metadata_mapping_file
+    CONTENTS_FILE = "contents"
 
-	CONTENTS_FILE = "contents"
-
-	def initialize(export_top_folder:,
-                 admin_set_id: AdminSet::DEFAULT_ID,
-                 metadata_mapping_file:  MetadataFileReader::DEFAULT_METADATA_MAPPING_FILE)
+    def initialize(export_top_folder:,
+                   admin_set_id: AdminSet::DEFAULT_ID,
+                   metadata_mapping_file:  MetadataFileReader::DEFAULT_METADATA_MAPPING_FILE)
       @admin_set_id = admin_set_id
       @export_top_folder = export_top_folder
       @metadata_mapping_file = metadata_mapping_file
-	end
+    end
 
-	def call
+    def call
       get_child_folders.each do |dir|
-      	Scholrax::Importer::WorkImporterJob.perform_later(export_path: dir.to_s, 
-                                                          admin_set_id: admin_set_id, 
-                                                          metadata_mapping_file: metadata_mapping_file)
+        Scholrax::Importer::WorkImporterJob.perform_later(export_path: dir.to_s, 
+                                                            admin_set_id: admin_set_id, 
+                                                            metadata_mapping_file: metadata_mapping_file)
       end
     end
 
